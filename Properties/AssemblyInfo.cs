c0a88cf660ce0a878d5c396716e93154dac1b58b
atenfyr <atenefyr@gmail.com>
2021-03-18 17:01:26 -0500

    copyright

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UAssetGUI")]
-[assembly: AssemblyCopyright("Copyright © Atenfyr 2020")]
+[assembly: AssemblyCopyright("Copyright © Atenfyr 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

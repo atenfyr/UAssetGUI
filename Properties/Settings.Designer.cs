a862524974063c1c3fb45c18a393370ecfe7a811
atenfyr <atenefyr@gmail.com>
2022-10-28 18:41:19 -0500

    embed resources into main executable

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,10 +8,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace UAssetGUI.Properties
-{
-
-
+namespace UAssetGUI.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
